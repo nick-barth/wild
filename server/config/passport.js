4863f1da2b3426ad4fcc2aa0bb685acc855c502a
Nick Barth <barth.nicholas@gmail.com>
2016-07-17 16:45:13 +0200

    figured some more stuff out, fixed schema

--- server/config/passport.js
+++ server/config/passport.js
@@ -39,8 +39,7 @@ module.exports = function (passport) {
 		passwordField: 'password',
 		passReqToCallback: true // allows us to pass back the entire request to the callback
 	},
-		function (req, email, password, done) {
-			console.log('wow');
+		function (req, username, password, done) {
 			// asynchronous
 			// User.findOne wont fire unless data is sent back
 			process.nextTick(function () {
@@ -48,7 +47,7 @@ module.exports = function (passport) {
 				// find a user whose email is the same as the forms email
 				// we are checking to see if the user trying to login already exists
 				User.findOne({
-					'local.email': email
+					'local.username': username
 				}, function (err, user) {
 					// if there are any errors, return the error
 					if (err) {
@@ -64,9 +63,8 @@ module.exports = function (passport) {
 						var newUser = new User();
 
 						// set the user's local credentials
-						newUser.local.email = email;
+						newUser.local.username = username;
 						newUser.local.password = newUser.generateHash(password);
-
 						// save the user
 						newUser.save(function (err) {
 							if (err) {
@@ -78,6 +76,44 @@ module.exports = function (passport) {
 
 				});
 
+			});
+
+		}));
+
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for signup
+	// by default, if there was no name, it would just be called 'local'
+
+	passport.use('local-login', new LocalStrategy({
+			// by default, local strategy uses username and password, we will override with email
+			usernameField: 'username',
+			passwordField: 'password',
+			passReqToCallback: true // allows us to pass back the entire request to the callback
+		},
+		function (req, username, password, done) { // callback with email and password from our form
+			// find a user whose email is the same as the forms email
+			// we are checking to see if the user trying to login already exists
+			User.findOne({
+				'local.username': username
+			}, function (err, user) {
+				// if there are any errors, return the error before anything else
+				if (err) {
+					return done(err);
+				}
+				// if no user is found, return the message
+				if (!user) {
+					console.log('oh no');
+					return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+				}
+				// if the user is found but the password is wrong
+				if (!user.validPassword(password)) {
+					console.log('bad pw');
+					return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+				}
+				// all is well, return successful user
+				return done(null, user);
 			});
 
 		}));
